7784295a99970c3b18d21c8fd80282c7fbf227ad
Bitterblue24 <taylorguasta24@gmail.com>
2025-02-17 15:33:40 -0500

    Create server.py
    not currently functional

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    response_copy = response.copy()
+    dominant_emotion = response_copy.pop('dominant_emotion')  # Get and remove
+    return f"For the given statement, the system response is {response_copy}. The dominant emotion is {dominant_emotion}."
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
