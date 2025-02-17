5b6dfc56f22a4157f569c488606b7a87dead1ec0
Bitterblue24 <taylorguasta24@gmail.com>
2025-02-17 13:07:10 -0500

    Update emotion_detection.py
    formatted output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,11 +1,21 @@
-import requests, json 
+import requests, json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = response.json()
+
+    emotion_predictions = formatted_response["emotionPredictions"]
+    top_prediction = emotion_predictions[0]
+    emotions = top_prediction["emotion"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+    output = emotions.copy()
+    output['dominant_emotion'] = dominant_emotion
+    return output
+
 
 
 #in terminal:
@@ -15,3 +25,4 @@ def emotion_detector(text_to_analyse):
     #python3.11 -m pip install requests
     #from emotion_detection import emotion_detector
     #emotion_detector("I love this new technology")
+    #print(json.dumps(result, indent=4)) 
