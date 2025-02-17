6e001094be539f9bea8b7194201cdf299be40491
Bitterblue24 <taylorguasta24@gmail.com>
2025-02-17 12:23:15 -0500

    Update emotion_detection.py
    #terminal instructions

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,3 +6,12 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
     return response.text
+
+
+#in terminal:
+    #git clone
+#in integrated terminal
+    #export PS1="[\[\033[01;32m\]\u\[\033[00m\]: \[\033[01;34m\]\W\[\033[00m\]]\$ "
+    #python3.11 -m pip install requests
+    #from emotion_detection import emotion_detector
+    #emotion_detector("I love this new technology")
